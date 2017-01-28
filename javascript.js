158674332710d18bce9a290071e71174ee30d276
Roberto <roberto.sousacordeiro@gmail.com>
2017-01-27 22:50:16 -0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  var myAPI = "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={32fd231b6abb27c5dc1db12d81aa2bae}";
+  var coords = "api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}";
+});
