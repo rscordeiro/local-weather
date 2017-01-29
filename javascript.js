79ed5a3c019bd09d995e0d92351dc495b44df1eb
Roberto <roberto.sousacordeiro@gmail.com>
2017-01-29 21:59:43 -0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -7,20 +7,45 @@ $(document).ready(function(){
   $.getJSON(coordAPI, function(data) {
     lat = data.geoplugin_latitude;
     lon = data.geoplugin_longitude;
-    $("#location-div").html("<p>" + data.geoplugin_region + ", " + data.geoplugin_countryName + "</p>")
-    $("#calendar-div").html(Date());
+    $("#location-div").html("<p>" + data.geoplugin_region + ", " + data.geoplugin_countryName + "</p>");
 
-    var myAPI = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +  "&APPID=" + apiKey + "&units=metric";
+    var myAPI = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +  "&APPID=" + apiKey;
 
     $.getJSON(myAPI, function(data){
       whatWeather(data);
     });
 
     var whatWeather = function(conditions) {
-      var tempe = "<p>" + conditions.main.temp + "</p>";
+      var tempe = conditions.main.temp;
       var mainWeather = "<p>" + conditions.weather[0].main + "</p> <br> <p>" + conditions.weather[0].description + "</p>";
-      $("#temp-div").html(tempe);
       $("#display-div").html(mainWeather);
+
+
+      var tempF = ((tempe - 273.15) * 1.8 + 32);
+      var tempC = (tempe - 273.15);
+
+      var toCelcius = function() {
+        $("#p-temp").html(tempC);
+      }
+
+      var toFahrenheit = function() {
+        $("#p-temp").html(tempF);
+      }
+
+      $("#p-temp").html(toCelcius());
+
+      $("#c-btn").click(function(){
+        $("#p-temp").html(toCelcius());
+        $("#c-btn").addClass("active");
+        $("#f-btn").removeClass("active");
+      });
+
+      $("#f-btn").click(function(){
+        $("#p-temp").html(toFahrenheit());
+        $("#f-btn").addClass("active");
+        $("#c-btn").removeClass("active");
+      });
+
     };
   });
 
